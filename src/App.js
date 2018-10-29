3c06ffc6a79152e265f2a754c3b882d97dfd07e6
Jared <kuil5230@gmail.com>
2018-10-29 09:49:40 +0800

    .

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 //現在稱為function component
-function App() {
+export default function App() {
+  const name = useFormInput("Jared");
+  const school = useFormInput("YUNTECH");
   const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    document.title = `You clicked ${count} times`;
-  });
+  const width = useWindow();
+  useDocumentTitle(`You clicked ${count} times`);
 
   return (
     <div>
+      name:
+      <input {...name} />
+      school:
+      <input {...school} />
       <p>You clicked {count} times</p>
       <button
         onClick={() => {
@@ -18,8 +23,44 @@ function App() {
         }}>
         Click me
       </button>
+      <p>
+        width:
+        {width}
+      </p>
     </div>
   );
 }
 
-export default App;
+function useFormInput(initialValue) {
+  const [value, setValue] = useState(initialValue);
+
+  function handleChange(e) {
+    setValue(e.target.value);
+  }
+
+  return {
+    value,
+    onChange: handleChange
+  };
+}
+
+function useDocumentTitle(title) {
+  useEffect(() => {
+    document.title = title;
+  });
+}
+
+function useWindow() {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleWidthResize = () => setWidth(window.innerWidth);
+
+    window.addEventListener("resize", handleWidthResize);
+    return () => {
+      window.removeEventListener("resize", handleWidthResize);
+    };
+  });
+
+  return width;
+}
